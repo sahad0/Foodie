b5d5873774e27fd5424ed327204783f0464a6642
Sahad <govardhan.n@codingmart.com>
2022-09-29 00:47:24 +0530

    comit

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 import RootNavigation from './navigation';
 
 
 export default function App() {
 
-
-
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
 
   return (
 
