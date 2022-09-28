8b8357f5c9e7b6fd411af143519890543504b259
Sahad <govardhan.n@codingmart.com>
2022-09-29 00:56:24 +0530

    commit

--- android/app/src/main/java/com/foodie/MainActivity.java
+++ android/app/src/main/java/com/foodie/MainActivity.java
@@ -1,11 +1,15 @@
 package com.foodie;
-
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-
+import org.devio.rn.splashscreen.SplashScreen; // here
 public class MainActivity extends ReactActivity {
-
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(null);
+    }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
