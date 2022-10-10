fc1a40a3a5391ae7d3483a5369807fdb5b551c1a
Sahad <sahadwg@gmail.com>
2022-10-10 12:44:06 +0530

    commit

--- screens/HomeV1_0.js
+++ screens/HomeV1_0.js
@@ -3,12 +3,14 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../Components/HeaderComponents/Home/Header'
 
 import BottomItems from '../Components/BodyComponents/Home/BottomItems'
-import { View } from 'react-native'
+import { Button, View } from 'react-native'
 import FOoter from '../Components/FooterComponent/Home/FOoter'
+import crashlytics from '@react-native-firebase/crashlytics';
 
 export default function HomeV1_0({navigation}) {
   return (
    <SafeAreaView style={{display:"flex",}}>
+    {/* <Button title='Hello' onPress={()=>{crashlytics().crash()}} /> */}
     <View style={{flexGrow:1}}>
       <Header navigation={navigation}/>
     </View>
